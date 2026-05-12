07c82ff1fb050951257178e35b65c6fff77f2014
rociocjz <rociocojulum@gmail.com>
2026-05-11 23:44:46 -0600

    Feat: creacion de Nodo con los atributos valor, hijos y altura

---
+++ models/nodo.py
@@ -0,0 +1,10 @@
+class Nodo:
+
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierdo = None
+        self.derecho = None
+        self.altura = 1
+
+        self.x = 0
+        self.y = 0
