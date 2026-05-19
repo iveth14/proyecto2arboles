767d57feea3b84df25faf17815f666bc349879bd
rociocjz <rociocojulum@gmail.com>
2026-05-18 19:54:35 -0600

    feat funciones

--- models/bst.py
+++ models/bst.py
@@ -46,3 +46,70 @@ class BST:
             return self._buscar(nodo.izquierdo, valor, camino)
         else:
             return self._buscar(nodo.derecho, valor, camino)
+        
+    def eliminar(self, valor):
+        self.raiz, eliminado = self._eliminar(self.raiz, valor)
+        return eliminado
+    
+    def _eliminar(self, nodo, valor):
+        if nodo is None:
+            return nodo, False
+        
+        eliminado = False
+
+        if valor < nodo.valor:
+            nodo.izquierdo, eliminado = self._eliminar(nodo.izquierdo, valor)
+        elif valor > nodo.valor:
+            nodo.derecho, eliminado = self._eliminar(nodo.derecho, valor)
+        else:
+            eliminado = True
+            if nodo.izquierdo is None and nodo.derecho is None:
+                return None, eliminado
+            if nodo.derecho is None:
+                return nodo.izquierdo, eliminado
+            
+            sucesor = self._minimo(nodo.derecho)
+            nodo.valor = sucesor.valor
+            nodo.derecho, _ = self._eliminar(nodo.derecho, sucesor.valor)
+        
+        return nodo, eliminado
+    
+
+    def preorden(self):
+        resultado = []
+        self._preorden(self.raiz, resultado)
+        return resultado
+    
+    def _preorden(self, nodo, resultado):
+        if nodo is None:
+            return
+        resultado.append(nodo)
+        self._preorden(nodo.izquierdo, resultado)
+        self._preorden(nodo.derecho, resultado)
+
+    def inorden(self):
+        resultado = []
+        self._inorden(self.raiz, resultado)
+        return resultado
+
+    def _inorden(self, nodo, resultado):
+        if nodo is None:
+            return
+        self._inorden(nodo.izquierdo, resultado)
+        resultado.append(nodo)
+        self._inorden(nodo.derecho, resultado)
+
+    def postorden(self):
+        resultado = []
+        self._postorden(self.raiz, resultado)
+        return resultado
+    
+    def _postorden(self, nodo, resultado):
+        if nodo is None:
+            return
+        self._postorden(nodo.izquierdo, resultado)
+        self._postorden(nodo.derecho, resultado)
+        resultado.append(nodo)
+
+
+        
